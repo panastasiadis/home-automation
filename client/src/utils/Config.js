31cad2b122795875c3336fd37043c836b9fdf531
paanastasiadis <panag1234@outlook.com>
2021-02-03 23:36:06 +0200

    Add wss support in server/client mqtt

--- client/src/utils/Config.js
+++ client/src/utils/Config.js
@@ -1,12 +1,5 @@
-function fixedEncodeURI(str) {
-  return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
-}
-
-const wsUrl = fixedEncodeURI(
-  'http://[2a02:2149:840f:3200:d85d:e680:e068:7e57]:8883'
-);
-
 export const backendApiUrl = {
-  server: 'http://192.168.1.66:5000/',
-  ws: 'http://192.168.1.66:8883',
+  server: 'http://localhost/',
+  ws: 'http://localhost:8883',
 };
+
